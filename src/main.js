8ec7da8133b3a6ac9f8fbdc6c107266a12b421fe
Guilherme Bassani <PC - Guilherme>
2025-06-21 10:11:33 -0300

    Forest Final

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import './style.css';
+
+const mobileBTN = document.querySelector('.js-btn');
+const mobileNav = document.querySelector('.js-mobileNav');
+
+mobileBTN.addEventListener('click', () => {
+    mobileNav.classList.toggle('hidden');
+    mobileNav.classList.toggle('flex');
+})
+
+mobileNav.addEventListener('click', () => {
+    mobileNav.classList.toggle('hidden');
+    mobileNav.classList.toggle('flex');
+})
+
+
+
+const video = document.querySelector('.js-video');
+const tempo = document.querySelector('.js-tempo');
+const temperatura = document.querySelector('.js-temperatura');
+const dia = document.querySelector('.js-dia');
+
+const numeroRandom = Math.floor(Math.random() * 10) + 20;
+temperatura.innerText = numeroRandom + "°";
+tempo.innerText = numeroRandom < 25 ? `🌧️${numeroRandom + 4}°` : `🌤️${numeroRandom + 4}°`;
+dia.innerText = new Date().toLocaleDateString("pt-BR", {
+    weekday: "long",
+});
+
+video.src = numeroRandom < 25 ? './public/imgs/video_chuva.mp4' : './public/imgs/video_sol.mp4';
